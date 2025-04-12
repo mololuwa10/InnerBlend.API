e45395baab04df880ac683dad3686e521b8559f2
Mololuwa Segilola <72552301+mololuwa10@users.noreply.github.com>
2025-04-12 01:25:57 +0100

    `Added .env to .gitignore, deleted User.cs model, and updated AssemblyInfo.cs version`

---
+++ Models/Authentication/LoginModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InnerBlend.API.Models.Authentication
+{
+    public class LoginModel
+    {
+        [Required]
+        public string? UsernameOrEmail { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
