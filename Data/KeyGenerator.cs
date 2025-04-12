b5c6b40cef26bba63be26ad11471e1c4c7c38cbb
Mololuwa Segilola <72552301+mololuwa10@users.noreply.github.com>
2025-04-12 01:37:27 +0100

    "Implemented a Key Generator in data folder"

---
+++ Data/KeyGenerator.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace InnerBlend.API.Data
+{
+    public class KeyGenerator
+    {
+        public static string GenerateKey() 
+        {
+            byte[] key = new byte[32];
+            RandomNumberGenerator.Fill(key);
+            
+            return Convert.ToBase64String(key);
+        }
+    }
+}
