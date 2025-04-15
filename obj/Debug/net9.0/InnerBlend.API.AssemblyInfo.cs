f3fc2d1e998e45178c6f95ee7da01a5e72405d43
Mololuwa Segilola <72552301+mololuwa10@users.noreply.github.com>
2025-04-15 22:25:28 +0100

    `Updated AssemblyInformationalVersionAttribute in InnerBlend.API project.`

--- obj/Debug/net9.0/InnerBlend.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/InnerBlend.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InnerBlend.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9859704456c20bf722bbde2352fbe73cd7c4443b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd154f9fb09a4d9186f5a99e5443fa315331ddda")]
 [assembly: System.Reflection.AssemblyProductAttribute("InnerBlend.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InnerBlend.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
